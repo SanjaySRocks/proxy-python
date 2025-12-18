3ff8e4222729d3e2d306d18dae9e5663887fa3f9
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2025-12-18 18:36:18 +0530

    Update rotating_proxy.py

--- rotating_proxy.py
+++ rotating_proxy.py
@@ -1,18 +1,16 @@
 import requests
 
-with open("validips.txt", "r") as f:
-    proxies = f.read().split("\n")
-
+with open("in_validips.txt", "r") as f:
+    proxies = [p for p in f.read().split("\n") if p.strip()]
 
 counter = 0
 for x in proxies:
     try:
         res = requests.get("http://ipinfo.io/json", proxies={"http":proxies[counter], "https":proxies[counter]})
-        print(res.status_code)
-        print(res.text)
+        print(proxies[counter], res.status_code)
 
     except:
-        print("Failed")
+        print("Failed: ", proxies[counter], res.status_code)
     finally:
         counter += 1
         counter % len(proxies)
